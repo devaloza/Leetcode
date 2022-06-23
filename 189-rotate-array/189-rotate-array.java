5016f063eb88c8e305604dec21d146d1afc08f1b
devaloza <devaloza93@gmail.com>
2022-06-23 10:51:19 +0100

    Time: 1 ms (84.31%), Space: 64.3 MB (59.82%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -3,34 +3,17 @@ class Solution {
         if (nums.length <=0) {
             return;
         }
-        k = k%nums.length;
-        reverse(nums, 0);
-        reverse(nums,k);
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums,0, k-1);
+        reverse(nums, k, nums.length - 1);
     }
-    private void reverse(int[] array, int index) {
-        int start = 0;
-        int end = array.length - 1;
-        if (index == 0) {
-            while (start < end) {
+    private void reverse(int[] array, int start, int end) {
+        while (start < end) {
                 swap(array, start, end);
                 start++;
                 end--;
             }
-        } else {
-            end = index-1;
-            while (start < end) {
-                swap(array, start, end);
-                start++;
-                end--;
-            }
-            start = index;
-            end = array.length - 1;
-             while (start < end) {
-                swap(array, start, end);
-                start++;
-                end--;
-            }
-        }
     }
     private void swap(int[] array2, int start, int end) {
         int temp = array2[start];
