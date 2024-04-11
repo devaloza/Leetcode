8ac2038680cb1fa1fbb64d0ccb310254fe3c90fd
Deval Oza <deval@Devals-Air.fritz.box>
2024-04-11 19:24:26 +0100

    Runtime 3ms beats 97.39% Memory 43.99mb beats 90.22%

--- 169-Majority-Element/169-Majority-Element.java
+++ 169-Majority-Element/169-Majority-Element.java
@@ -1,8 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        if (nums.length <= 2) return nums[0];
-        int j = nums.length / 2;
-        return nums[j];
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
     }
 }
