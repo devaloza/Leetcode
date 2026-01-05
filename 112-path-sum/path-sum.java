0a52d4c348c80ce91b42ee30f08a6a0a872b68be
Deval <devaloza93@gmail.com>
2026-01-05 21:20:18 +0000

    Time: 0 ms (100.00%) | Memory: 45.2 MB (14.89%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -16,13 +16,9 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) return false;
-        System.out.printf("Root val and target sum is %d and %d", root.val, targetSum);
-        System.out.println("-");
         if (root.left == null && root.right == null && targetSum-root.val == 0) {
-            System.out.print("--");
             return true;
         }
-        System.out.println(targetSum);
         return ((hasPathSum(root.left, targetSum-root.val))|| (hasPathSum(root.right, targetSum-root.val))); 
     }
 }
