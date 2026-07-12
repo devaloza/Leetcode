a29289402df9c0f80e856471d4c0c8fc4e4dd733
Deval <devaloza93@gmail.com>
2026-07-12 15:36:47 +0100

    Time: 1 ms (84.35%) | Memory: 46.3 MB (65.31%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -1,21 +1,18 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         boolean[] seen = new boolean[rooms.size()];
-        seen[0] = true;
-        Stack<Integer> stack = new Stack();
-        stack.push(0);
-        while (!stack.isEmpty()) {
-            int node = stack.pop();
-            for (int n:rooms.get(node)) {
-                if (!seen[n]) {
-                    seen[n] = true;
-                    stack.push(n);
-                }
-            }
-        }
-        for (boolean v: seen) {
-            if (!v) return false;
+        dfs(0, rooms, seen);
+        for (int i =0; i < rooms.size(); i++) {
+            if (!seen[i]) return false;
         }
         return true;
+    }
+    public void dfs(int room, List<List<Integer>> roomList, boolean[] seen) {
+        seen[room] = true;
+        for (int i: roomList.get(room)) {
+            if (!seen[i]) {
+                dfs(i, roomList, seen);
+            }
+        }
     }
 }
